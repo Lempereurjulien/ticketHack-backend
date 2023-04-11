61d7fd5920299aaf3436fad9848687a3185d7283
Lempereur <lempereur.julien83@gmail.com>
2023-04-11 12:00:32 +0200

    Creation collection cart && booking

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const fetch = require('node-fetch');
+const Trip = require('../models/trips');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/tripsFilter', (req, res)=>{
+Trip.find({departure : req.body.departure, arrival : req.body.arrival, date : req.body.date})
+.then(data => {
+  res.json({trips : data});
+});
+
+}
+)
 module.exports = router;
