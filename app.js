2d54f9e96cf647790eb6d3bcc2bff46fb632ba40
Lempereur <lempereur.julien83@gmail.com>
2023-04-11 15:38:06 +0200

    route back ok

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 require('./models/connection');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 
 var app = express();
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
