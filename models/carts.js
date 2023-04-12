9a7b34ca808993597bf9112ede1cd8ca9d179121
Carole Bellet <belletcarole@yahoo.fr>
2023-04-12 10:33:03 +0200

    cart-backend

--- models/carts.js
+++ models/carts.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const Trip = require('../models/trips');
+
 
 const cartSchema = mongoose.Schema({
     panier: { type: mongoose.Schema.Types.ObjectId, ref: 'trips' },
